69092c738890309551bf22e7ebea739d3c7c6875
zemdam <az430277@students.mimuw.edu.pl>
2023-01-23 19:27:22 +0100

    Prob working

--- app/models/storage.rb
+++ app/models/storage.rb
@@ -4,12 +4,6 @@ class Storage < ApplicationRecord
   validates :name, :available_cheese, :available_ham, :available_sauce, :available_pineapple, :pizzeria_point, presence: true
   validates :name, uniqueness: { scope: :pizzeria_point }, on: :create
 
-  def diffrent_name?
-    puts name
-    puts "TUUUTTTTAAAAAAJJJJJJJJJ"
-    name != "Normall Storage 1"
-  end
-
   def self.search(search)
     if search
         where('name LIKE ?', "%#{search}%")
